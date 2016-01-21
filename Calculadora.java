09aa3039585e547149444f7012610c34369452f0
liveworldstrike <lazodellamas12@gmail.com>
2016-01-21 11:08:34 +0100

    metodo que suma los números comprendidos entre el 0 y el 10 (ambos incluidos)

--- Calculadora.java
+++ Calculadora.java
@@ -15,19 +15,33 @@ public class Calculadora
      */
     public Calculadora()
     {
-        
-        
-    }
 
+    }
+    
     /**
      * metodo que inprime los multiplos (5 primeros)entre 10-95 
      */
     public void multiplesOfFive()
     {
-       multi = 10;
-       while(multi < 90){
-           multi = multi +5;
-           System.out.println(multi);
+        multi = 10;
+        while(multi < 90){
+            multi = multi +5;
+            System.out.println(multi);
         }
+    }
+
+    /**
+     * metodo que imprime por pantalla la suma de los números comprendidos entre el 0 y el 10 (ambos incluidos)
+     */
+    public void sumValues()
+    {
+        int sum = 0;
+        int cont = 0;
+        while (cont<10){
+            cont = cont+1 ;
+            sum = sum + cont ;
+
+        }
+        System.out.println("la suma de los números comprendidos entre el 0 y el 10 (ambos incluidos)" + " = " + sum);
     }
 }
