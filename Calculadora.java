dd0c3b4abbbaa443dc8cf0f5934684f0ba59665c
liveworldstrike <lazodellamas12@gmail.com>
2016-01-22 13:33:54 +0100

    solucionado error en la obcion del nuevo metodo del if

--- Calculadora.java
+++ Calculadora.java
@@ -99,7 +99,7 @@ public class Calculadora
     public int mult(int a ,int b)
     {
         int multi = -1;
-        if (a<0 && b<0){
+        if (a<0 || b<0){
             System.out.println("error valores mal introducidos " );
 
         }
